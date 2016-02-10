e8b721006d5c3ae8472eb49821247b5a4a4d4dcf
Roman Kaspar <roman@salsitasoft.com>
2016-02-11 00:34:43 +0100

    remove spec files for cmdline and task modules

--- code/lib/cmdline.spec.js
+++
@@ -1,7 +0,0 @@
-import assert from 'assert';
-
-describe('Command line module', () => {
-  it('# TODO add tests', () => {
-    assert.equal(1, 1);
-  });
-});
--- code/lib/task.spec.js
+++
@@ -1,7 +0,0 @@
-import assert from 'assert';
-
-describe('Task module', () => {
-  it('# TODO add tests', () => {
-    assert.equal(1, 1);
-  });
-});
