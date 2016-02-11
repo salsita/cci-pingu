1a369d805cd248bec0056c878e1cf749cc765193
Roman Kaspar <roman@salsitasoft.com>
2016-02-11 13:04:43 +0100

    add stub for task module unit tests

---
+++ code/lib/task.spec.js
@@ -0,0 +1,6 @@
+// The task module holds the application logic, however is completely
+// asynchronous and I/O heavy. In order to write proper unit tests, a lot of
+// services would have to be mocked.
+// ... I currently don't have time to do so, however, feel free to write the
+// mocks and add missing unit tests, send the pull request, and I'll be more
+// than happy to merge them in.
