5c200cc3b1db17a33ebc3cd37b7b6b0564de3458
Roman Kaspar <roman@salsitasoft.com>
2017-10-18 23:02:40 +0200

    prepare for npm publish

---
+++ cci-pingu.js
@@ -0,0 +1,27 @@
+import logger from './lib/logger';
+import cmdline from './lib/cmdline';
+import cfgFile from './lib/cfg-file';
+import task from './lib/task';
+import version from './lib/version';
+
+const main = () => {
+  logger.install();
+  const options = cmdline.options;
+  if (options.silent) { logger.level('error'); }
+  if (options.debug)  { logger.level('log'); }
+
+  console.info(`CCI-PINGU started (version:${version})`);
+  console.log('Command line options:\n' + JSON.stringify(options, null, 4));
+
+  const sigHandler = () => {
+    console.info('SIGINT / SIGTERM received, CCI-PINGU terminating. Thanks for flying with us!');
+    process.exit(0);
+  };
+  process.on('SIGINT', sigHandler);
+  process.on('SIGTERM', sigHandler);
+
+  options.cfgFile = cfgFile.setFilename(options.config);
+  task.run(options);
+};
+
+module.exports = main; // for the runner in bin directory
